3409ea0e1a80661c8fed0438a2ec0a7c6a3a64e5
saminfatima20 <saminfatima20@gmail.com>
2025-09-17 11:40:00 +0000

    numpy project 5

---
+++ numpy .py
@@ -0,0 +1,7 @@
+arr = np.array([1, 2, 3, 4, 5])
+
+print("Original Array:", arr)
+print("Mean:", np.mean(arr))
+print("Standard Deviation:", np.std(arr))
+print("Sum:", np.sum(arr))
+print("Squared Array:", arr**2)
